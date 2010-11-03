bb37bc871c26de4de8f8fae16028abbd00112101
Gomita <gomita@xuldev.org>
2010-11-04 01:38:07 +0900

    add pref to enable modifying preview size (part2)

--- defaults/preferences/tabscope-prefs.js
+++ defaults/preferences/tabscope-prefs.js
@@ -2,3 +2,4 @@ pref("extensions.tabscope.popup_alignment", 2);
 pref("extensions.tabscope.popup_delay", 300);
 pref("extensions.tabscope.animate", 2);
 pref("extensions.tabscope.hovering", true);
+pref("extensions.tabscope.preview_width", 240);
